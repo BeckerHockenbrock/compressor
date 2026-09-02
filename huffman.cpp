7188feb74ab6d650bae55024754ab8acd0307685
Becker H <bhocken91@gmail.com>
2026-09-02 12:01:25 -0700

    moved everything over to seperate huffman file for future other compression algrothims

---
+++ huffman.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+class HuffmanCompression {
+public:
+    map<char, int> getCharFrequency(string filename) {
+    
+        ifstream file(filename);
+        if (!file.is_open()) {
+            cout << "Error opening file" << endl;
+            return map<char, int>();
+        }
+        
+        string line;
+        cout << "\n";
+        map<char, int> charFrequency;
+        while (getline(file, line)) {
+            for (char c : line) {
+                charFrequency[c]++;
+            }
+        }
+        
+        file.close();
+        return charFrequency;
+    }
+    
+    vector<pair<char, int>> sortMap(const map<char, int>& charFrequency) {
+        // 1. Copy map entries into a vector of pairs
+        vector<pair<char, int>> sortedFreq(charFrequency.begin(), charFrequency.end());
+        // 2. Sort the vector by frequency (pair.second) in ascending order
+        sort(sortedFreq.begin(), sortedFreq.end(), [](const auto& a, const auto& b) {
+            return a.second < b.second;
+        });
+    
+        // 3. Return the sorted vector!
+        return sortedFreq;
+    }
+    
+    void compress(string filename) {
+        map<char, int> charFrequency = getCharFrequency(filename);
+        vector<pair<char, int>> sortedFreq = sortMap(charFrequency);
+    
+        for (auto& pair : sortedFreq) {
+            cout << pair.first << " " << pair.second << endl;
+        }
+    
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,58 +1,14 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <map>
-#include <vector>
-#include <algorithm>
+#include "huffman.cpp"
 
 using namespace std;
 
 
-map<char, int> getCharFrequency(string filename) {
-    
-    ifstream file(filename);
-    if (!file.is_open()) {
-        cout << "Error opening file" << endl;
-        return map<char, int>();
-    }
-    
-    string line;
-    cout << "\n";
-    map<char, int> charFrequency;
-    while (getline(file, line)) {
-        for (char c : line) {
-            charFrequency[c]++;
-        }
-    }
-    
-    file.close();
-    return charFrequency;
-}
-
-vector<pair<char, int>> sortMap(const map<char, int>& charFrequency) {
-    // 1. Copy map entries into a vector of pairs
-    vector<pair<char, int>> sortedFreq(charFrequency.begin(), charFrequency.end());
-    // 2. Sort the vector by frequency (pair.second) in ascending order
-    sort(sortedFreq.begin(), sortedFreq.end(), [](const auto& a, const auto& b) {
-        return a.second < b.second;
-    });
-
-    // 3. Return the sorted vector!
-    return sortedFreq;
-}
-
 int main() {
-    string filename;
-    cout << "Enter the filename: ";
-    cin >> filename;
-
-    map<char, int> charFrequency = getCharFrequency(filename);
-    vector<pair<char, int>> sortedFreq = sortMap(charFrequency);
-
-    for (auto& pair : sortedFreq) {
-        cout << pair.first << " " << pair.second << endl;
-    }
-
-    return 0;
     
+    HuffmanCompression huffman;
+    huffman.compress("test.txt");
+    return 0;
 }
